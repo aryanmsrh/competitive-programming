ed732cdf129286b6c77a636ad1298e87323720a1
Aryan Mishra <aryanmsrh@gmail.com>
2026-09-15 15:14:20 +0530

    restructure: folders and files

---
+++ codeforces/problemsets/800/2259B.py
@@ -0,0 +1,24 @@
+"""
+Codeforces Round 1119 (Div. 3)
+Problem: 2259B - MINUS TWO
+Link: https://codeforces.com/problemset/problem/2259/B
+Rating: 800
+Tags: math, number theory
+Key Idea: 
+"""
+
+
+import sys
+input = sys.stdin.readline
+
+def solve():
+    pass
+
+def main():
+    line = input()
+    t = int(line.strip())
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
---
+++ templates/python_fastio.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    pass
+
+def main():
+    line = input()
+    t = int(line.strip())
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
